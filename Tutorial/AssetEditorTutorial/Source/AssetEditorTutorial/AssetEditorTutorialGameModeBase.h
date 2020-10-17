d84d55cdd3ea9e7df9999676f1f4c152909e5e9f
OlivierArgentieri <olivierargentieri@gmail.com>
2020-10-17 15:11:05 +0200

    Create Tutorial Project

---
+++ Tutorial/AssetEditorTutorial/Source/AssetEditorTutorial/AssetEditorTutorialGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AssetEditorTutorialGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASSETEDITORTUTORIAL_API AAssetEditorTutorialGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Tutorial/AssetEditorTutorial/Source/AssetEditorTutorialEditor.Target.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class AssetEditorTutorialEditorTarget : TargetRules
+{
+	public AssetEditorTutorialEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange( new string[] { "AssetEditorTutorial" } );
+	}
+}
