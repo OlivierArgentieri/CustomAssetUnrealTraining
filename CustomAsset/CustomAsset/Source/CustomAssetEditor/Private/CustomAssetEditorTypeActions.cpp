76df9845b9d6da2b836524a870a4927e81cf70d4
OlivierArgentieri <olivierargentieri@gmail.com>
2020-10-19 08:58:06 +0200

    fill CustomAssetEditorTypeActions.cpp
    need to link type actions and module

--- CustomAsset/CustomAsset/Source/CustomAssetEditor/Private/CustomAssetEditorTypeActions.cpp
+++ CustomAsset/CustomAsset/Source/CustomAssetEditor/Private/CustomAssetEditorTypeActions.cpp
@@ -1,3 +1,35 @@
 #include "CustomAssetEditor/Classes/CustomAssetEditorTypeActions.h"
 
+#include "CustomAssetEditor.h"
 
+#define LOCTEXT_NAMESPACE "CustomAssetEditor"
+
+
+UClass* FAssetTypeActions_CustomAssetEditor::GetSupportedClass() const
+{
+	return UCustomAssetEditor::StaticClass();
+}
+
+FText FAssetTypeActions_CustomAssetEditor::GetName() const
+{
+	return LOCTEXT("AssetTypeActions_CustomAssetEditor", "Custom Asset Editor");
+}
+
+FColor FAssetTypeActions_CustomAssetEditor::GetTypeColor() const
+{
+	return FColor(128, 128, 128);
+}
+
+
+uint32 FAssetTypeActions_CustomAssetEditor::GetCategories()
+{
+	return EAssetTypeCategories::Misc;
+}
+
+void FAssetTypeActions_CustomAssetEditor::GetActions(const TArray<UObject*>& InObjects, FMenuBuilder& MenuBuilder)
+{
+	// todo 
+}
+
+
+#undef LOCTEXT_NAMESPACE
